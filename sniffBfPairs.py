1ca8742425290c59adea8540c81eee0e3ca3785c
vitowu <chenhaowu@link.cuhk.edu.cn>
2023-02-05 22:01:54 +0800

    add application section; integrate testings

--- sniffBfPairs.py
+++ sniffBfPairs.py
@@ -5,12 +5,13 @@ import time
 
 IFACE = "wlan0"
 
-# def sniff_handler(pkt):
-#     if pkt.haslayer(Dot11NDPA):
-#         print(f"name: {pkt[Dot11NDPA].name}"
-#               f"\t addr1: {pkt[Dot11].addr1}\t addr2: {pkt[Dot11].addr2}\t addr3: {pkt[Dot11].addr3}")
-#         print(bytes_hex(pkt))
-#     return pkt
+
+def sniff_handler(pkt):
+    if pkt.haslayer(Dot11NDPA):
+        print(f"name: {pkt[Dot11NDPA].name}"
+              f"\t addr1: {pkt[Dot11].addr1}\t addr2: {pkt[Dot11].addr2}\t addr3: {pkt[Dot11].addr3}")
+        print(bytes_hex(pkt))
+    return pkt
 
 
 # class TimerThread(Thread):
@@ -31,13 +32,13 @@ IFACE = "wlan0"
 
 # sniff(iface=IFACE, prn=sniff_handler, filter='type ctl')
 
-# source = SniffSource(iface=IFACE, filter="type ctl")
-# queue_sink = QueueSink()
-# console_sink = ConsoleSink()
-# source > TransformDrain(sniff_handler) > console_sink
-# p = PipeEngine(source)
-# p.start()
-# p.wait_and_stop()
+source = SniffSource(iface=IFACE, filter="type ctl")
+queue_sink = QueueSink()
+console_sink = ConsoleSink()
+source > TransformDrain(sniff_handler) > console_sink
+p = PipeEngine(source)
+p.start()
+p.wait_and_stop()
 
 # stop_event = Event()
 # t = TimerThread(interval=1, func=timer_function, event=stop_event)
@@ -45,9 +46,9 @@ IFACE = "wlan0"
 # time.sleep(10)
 # stop_event.set()
 
-frame = RadioTap() / Dot11(addr1="ff:ff:ff:ff:ff:ff",
-                           addr2="00:01:02:03:04:05",
-                           addr3="00:01:02:03:04:05") / \
-        Dot11Beacon(cap="ESS", timestamp=1) / \
-        Dot11Elt(ID="SSID", info="123123123")
-sendp(frame, iface="wlan0", loop=1, inter=0.001)
+# frame = RadioTap() / Dot11(addr1="ff:ff:ff:ff:ff:ff",
+#                            addr2="00:01:02:03:04:05",
+#                            addr3="00:01:02:03:04:05") / \
+#         Dot11Beacon(cap="ESS", timestamp=1) / \
+#         Dot11Elt(ID="SSID", info="123123123")
+# sendp(frame, iface="wlan0", loop=1, inter=0.001)
